c8f55f31bc936c3ecf90221f4c2b3f127dfe06cc
Ronald-Tuco <tj.tucoronald@gmail.com>
2017-02-07 17:17:46 -0400

    Update models.py

--- clinic/models.py
+++ clinic/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Doctor(models.Model):
+    user = models.OneToOneField(User, primary_key=True)
 
+    def __str__(self):
+        return self.user.first_name + ' ' + self.user.last_name
+        
 class Patient(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
@@ -10,8 +16,17 @@ class Patient(models.Model):
     phone = models.CharField(max_length=100)
     cellphone = models.CharField(max_length=100)
     background = models.CharField(max_length=200)
-    
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
     def __str__(self):
     	return self.last_name + ', ' + self.first_name
 
-# Create your models here.
+class Visit(models.Model):
+    date = models.DateTimeField()
+    patient = models.ForeignKey(Patient)
+    doctor = models.ForeignKey(Doctor)
+    comments = models.CharField(max_length=2000)
