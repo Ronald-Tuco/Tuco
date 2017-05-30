37540b4908e5564649e289647e6d75371c04ade4
Ronald-Tuco <tj.tucoronald@gmail.com>
2017-05-30 15:53:01 -0400

    Update models.py

--- clinic/models.py
+++ clinic/models.py
@@ -9,35 +9,40 @@ class Doctor(models.Model):
         return self.user.first_name + ' ' + self.user.last_name
         
 class Patient(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
+    first_name = models.CharField("Nombre", max_length=200)
+    last_name = models.CharField("Apellido", max_length=200)
     birth_date = models.DateField()
-    ci = models.CharField(max_length=50)
-    occupation = models.CharField(max_length=200)
-    phone = models.CharField(max_length=100)
-    cellphone = models.CharField(max_length=100)
-    background = models.CharField(max_length=200)
+    ci = models.CharField("C.I.", max_length=50)
+    occupation = models.CharField("Ocupacion", max_length=200)
+    phone = models.CharField("Telefono", max_length=100)
+    cellphone = models.CharField("Celular", max_length=100)
+    background = models.CharField("Antecedentes", max_length=200)
     GENDER_CHOICES = (
-        ('M', 'Male'),
-        ('F', 'Female'),
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
     )
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    gender = models.CharField("Sexo", max_length=1, choices=GENDER_CHOICES)
 
     def __str__(self):
     	return self.last_name + ', ' + self.first_name
 
 class Visit(models.Model):
-    BACKGROUND_CHOICES = (
-        ('first time', 'first time'),
-        ('Astenopia', 'Astenopia'),
-        ('Glasses broken', 'Glasses broken'),
-        ('Uses just G', 'Uses just G'),
-        ('Uses just C', 'Uses just C'),
-        ('Bifocales', 'Bifocales')
-    )
-    title = Multiselectfield(choices=BACKGROUND_CHOICES)
-    date = models.DateTimeField()
-    patient = models.ForeignKey(Patient)
     doctor = models.ForeignKey(Doctor)
-    
+    patient = models.ForeignKey(Patient)
+    date = models.DateTimeField()
+    BACKGROUND_CHOICES = (
+        ('first time', 'Primera Vez'),
+        ('Glasses broken', 'Rotura de lentes'),
+        ('Lost Glasses', 'Extravio de Lentes'),
+        ('Uses LC', 'Lentes de Contacto'),
+        ('Uses just G', 'Usa solo L'),
+        ('Uses just C', 'Usa solo C'),
+        ('Bifocales', 'Bifocales'),
+        ('RGP', 'RGP'),
+        ('Astenopia', 'Astenopia'),
+        ('Progresive', 'Progresivas'),
+        ('Hidrofilas', 'Hidrofilas')
+    )
+    title = MultiSelectField("Consulta", choices=BACKGROUND_CHOICES, null=True)  
 comments = models.CharField(max_length=2000)
+# Create your models here.
