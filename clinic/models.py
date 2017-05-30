bf308cb6ea2f89f673b542b044237bdb50109f32
Ronald-Tuco <tj.tucoronald@gmail.com>
2017-05-30 16:48:15 -0400

    Update models.py

--- clinic/models.py
+++ clinic/models.py
@@ -27,22 +27,29 @@ class Patient(models.Model):
     	return self.last_name + ', ' + self.first_name
 
 class Visit(models.Model):
-    doctor = models.ForeignKey(Doctor)
-    patient = models.ForeignKey(Patient)
-    date = models.DateTimeField()
+    doctor = models.ForeignKey(Doctor, verbose_name="Doctor")
+    patient = models.ForeignKey(Patient, verbose_name="Paciente")
+    date = models.DateTimeField("Fecha")
     BACKGROUND_CHOICES = (
-        ('first time', 'Primera Vez'),
-        ('Glasses broken', 'Rotura de lentes'),
-        ('Lost Glasses', 'Extravio de Lentes'),
-        ('Uses LC', 'Lentes de Contacto'),
-        ('Uses just G', 'Usa solo L'),
-        ('Uses just C', 'Usa solo C'),
-        ('Bifocales', 'Bifocales'),
-        ('RGP', 'RGP'),
-        ('Astenopia', 'Astenopia'),
-        ('Progresive', 'Progresivas'),
-        ('Hidrofilas', 'Hidrofilas')
+        ('first_time', 'Primera Vez'),
+        ('broken_lasses', 'Rotura de lentes'),
+        ('lost_glasses', 'Extravio de Lentes'),
+        ('uses_lc', 'Lentes de Contacto'),
+        ('uses_jf', 'Usa solo L'),
+        ('uses_jn', 'Usa solo C'),
+        ('bifocales', 'Bifocales'),
+        ('rgp', 'RGP'),
+        ('astenopia', 'Astenopia'),
+        ('progresive', 'Progresivas'),
+        ('hidrofilas', 'Hidrofilas')
     )
     title = MultiSelectField("Consulta", choices=BACKGROUND_CHOICES, null=True)  
+    mc = models.CharField("Motivo de la Consulta", max_length=2000, null=True)
+    app = models.CharField("APP", max_length=2000, null=True)
+    apf = models.CharField("APF", max_length=2000, null=True)
+    od = models.CharField("OD", max_length=50, null=True)
+    ol = models.CharField("OL", max_length=50, null=True)
+    convergencia = models.CharField("Convergencia", max_length=50, null=True)
+    eyere = models.CharField("Reflejos pupilares", max_length=100, null=True)
 comments = models.CharField(max_length=2000)
 # Create your models here.
